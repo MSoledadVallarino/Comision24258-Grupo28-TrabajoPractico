d8b67ddcbdd2017bf2064d887b0a5b8c00b38cdb
carogigena <gigena.carola@gmail.com>
2024-05-10 00:32:58 -0300

    Mejora del Modal de contacto. Y se agregó el btn Ingresar a todas las paginas.

--- js/script.js
+++ js/script.js
@@ -3,18 +3,36 @@ let contactForm = document.getElementById("contactForm");
 contactForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const ele = document.forms["contactForm"];
-  let datosCompletos=true;
-for (let i = 0; i < ele.length ;i++) {
-    if (ele.elements[i].type!="submit"){
-        if(ele.elements[i].value==""){
-            console.log(ele.elements[i].type);
-            ele.elements[i].style.backgroundColor="#e5bcfc";
-            datosCompletos=false;}
-        else ele.elements[i].style.backgroundColor="#e1eee1";
+  let datosCompletos = true;
+  // Obtiene el modal
+  var modal = document.getElementById("contactModal");
+
+  // Obtiene <span> elemento que cierra el modal
+  var span = document.getElementsByClassName("close")[1];
+
+  // Cuando hace click en <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // Cuando el usuario hace click en cualquier lugar de fuera del modal, lo cierra.
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
   }
-if (datosCompletos){
-    alert(`${nombre.value} su email se ha envíado exitosamente`);
+  for (let i = 0; i < ele.length; i++) {
+    if (ele.elements[i].type != "submit") {
+      if (ele.elements[i].value == "") {
+        ele.elements[i].style.backgroundColor = "#e5bcfc";
+        datosCompletos = false;
+      }
+      else { ele.elements[i].style.backgroundColor = "#e1eee1"; }
+    }
+  }
+  if (datosCompletos) {
+    $('#idHeader').text(`${nombre.value}`);
+    modal.style.display = "block";
     document.getElementById("contactForm").reset();
-}
+  }
 });
